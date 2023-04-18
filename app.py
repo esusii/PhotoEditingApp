e8b12332cecc9f34afe4c9c123ff9dca6f9aed43
esusii <129773516+esusii@users.noreply.github.com>
2023-04-18 18:55:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+import streamlit as st
+from PIL import Image
 
+st.markdown("<h1 style= 'text-align: center; '>Photo Editor</h1>', unsafe_allow_html=True)
+st.markdown("---")
+st.file_uploader("Upload Your Image", type=)            
